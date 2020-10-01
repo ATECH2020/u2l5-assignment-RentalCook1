8446b79c49b8d1e415b1afcb75a1c2ca934d7384
Vincent Carlton <vincent.236568@nv.ccsd.net>
2020-10-01 07:07:08 +0000

    did stuff

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,20 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+      //gathering ints
+      Scanner input = new Scanner(System.in); 
+      int taxRate = input.nextInt();
+      int boardNum = input.nextInt();
+      double windowNum = input.nextDouble();
+
+       Construction house = new  Construction(8, 11, taxRate);
+       double total =(house.lumberCost(boardNum) + house.windowCost(windowNum));
+       System.out.println(total);
+       System.out.print(house.grandTotal(total));
+       
+
+
+
+
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
 
+
 public class HowFarAway 
 {
     public static void main(String[] args)
     {
-        // your code here.
+
+      //Gathering input for computing
+      Scanner input = new Scanner(System.in); 
+      double startLat = input.nextDouble();
+      double startLong = input.nextDouble();
+      double endLat = input.nextDouble();
+      double endLong = input.nextDouble();
+
+      //geolocating stuff
+      GeoLocation start = new GeoLocation(startLat, startLong);
+      GeoLocation end = new GeoLocation(endLat, endLong);
+      double distance = start.distanceFrom(end);
+      System.out.print(distance);
+
     }
 }
